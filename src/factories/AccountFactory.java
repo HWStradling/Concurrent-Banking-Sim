52736fd54c26f4525e5468e495056b8e638401b2
HWStradling <99242469+HWStradling@users.noreply.github.com>
2024-11-14 02:50:36 +0000

    initial factories package structure.

---
+++ src/factories/AccountFactory.java
@@ -0,0 +1,10 @@
+package factories;
+
+import accounts.BankAccount;
+
+public class AccountFactory {
+    public BankAccount[] getBankAccounts(int length){
+        BankAccount[] accountsArray = new BankAccount[length];
+
+    }
+}
